9182964f0d2212894b819ba276dd28ed64bc80a5
Alexey Yanvarev <yanvarev_2015@mail.ru>
2021-04-05 00:10:04 +0400

    fix: fix res for logging

--- src/resources/authentication/signin.router.js
+++ src/resources/authentication/signin.router.js
@@ -6,10 +6,16 @@ const userService = require('../users/user.service');
 router.route('/').post(async (req, res) => {
   const auth = await userService.authenticate(req.body);
 
-  res.status(OK).json({
-    message: 'Authenticated',
-    ...auth
-  });
+  if (auth.error) {
+    res.status(OK).json({
+      ...auth
+    });
+  } else {
+    res.status(OK).json({
+      message: 'Authenticated',
+      ...auth
+    });
+  }
 });
 
 module.exports = router;
